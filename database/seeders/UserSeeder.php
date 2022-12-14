ef8e89ca65baeecd28bde645120c124697b05e4a
salnuraqidah <salnuraqidah@gmail.com>
2022-12-14 20:36:57 +0700

    seeding user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Salma',
+                'email' => 'salma@sal.com',
+                'password' => bcrypt('salamah2022'),
+                'remember_token' => null,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Nur',
+                'email' => 'nur@sal.com',
+                'password' => bcrypt('salamah2022'),
+                'remember_token' => null,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ]);
+    }
+}
